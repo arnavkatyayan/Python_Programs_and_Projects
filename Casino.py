fe951c0ea414992741ebd2671d714212ba6b5142
Arnav Katyayan <ishumisra99@gmail.com>
2024-09-30 17:55:05 +0530

    Added casino.py

---
+++ Casino.py
@@ -0,0 +1,20 @@
+#Casino Simple Project
+import random
+
+def checkCash(cash):
+    if(cash<10000):
+        print("Cant play the game")
+        return
+    else:
+        print("Can Play the game")
+        gameFunction()
+
+def gameFunction():
+    return ans
+
+
+
+
+name=input("Please enter your name")
+cash=float(input("Please enter the cash"))
+
