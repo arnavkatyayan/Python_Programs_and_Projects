affd2be4b84e4db57531a474851824728fb983a3
Arnav Katyayan <ishumisra99@gmail.com>
2024-10-02 12:34:42 +0530

    Problem Statment

---
+++ CoffeeMachine.py
@@ -0,0 +1,14 @@
+#Coffee Machine Project
+#Problem Statement-
+#Basically it will ask user espresso,latte,cappuchino and report
+#then the user will input what he wants
+#resources will be checked
+#If resources are sufficient the money will be asked
+#if the money is over the coffee amt it will be refunded
+#if everything goes right the user will get his coffee
+#For espresso - 50mlWater , 18g coffee,
+#For Latte = 200mlWater, 24gcofee,150ml milk
+#For cappucino = 250mlWater, 24gcofee,100ml milk.
+#penny=1c,Nickel=5C,Dime=10C,Quater=25C
+#Money for all three is 2.5,3.5,4 respectively.
+#Lets Build!
